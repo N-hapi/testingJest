b3a23101b689e098dafe2dd162da4f4abb0560e3
Nael <nael.shuwaikh@gmail.com>
2024-05-29 20:54:18 +0200

    add prettier etc

--- sum.spec.ts
+++ sum.spec.ts
@@ -1,15 +1,19 @@
-import { sum } from './sum';
+import { sum } from "./sum";
 
-describe('sum function', () => {
-  test('adds 1 + 2 to equal 3', () => {
+describe("sum function", () => {
+  test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  test('adds -1 + 1 to equal 0', () => {
+  test("adds -1 + 1 to equal 0", () => {
     expect(sum(-1, 1)).toBe(0);
   });
 
-  test('adds 0 + 0 to equal 0', () => {
+  test("adds 0 + 0 to equal 0", () => {
     expect(sum(0, 0)).toBe(0);
   });
+});
+
+describe("sum function", () => {
+  test("something", () => {});
 });
