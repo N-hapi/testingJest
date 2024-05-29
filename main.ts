5d6dc083c0d8938269c7f0e7df024e8f77ba1510
Nael <nael.shuwaikh@gmail.com>
2024-05-29 22:46:12 +0200

    added the line covrage

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ import { AppModule } from "./src/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(3000, "127.0.0.1"); // Specify '127.0.0.1' here
+  console.log(`Application is running on: http://127.0.0.1:3000`);
 }
 
 bootstrap();
